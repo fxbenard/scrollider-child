bff248823565494c756672879bdf4258e39992f2
Ulrich Pogson <grapplerulrich@gmail.com>
2014-10-09 13:03:30 +0200

    Load child theme textdomain

--- functions.php
+++ functions.php
@@ -13,37 +13,37 @@ define( 'SCROLLIDER_CHILD_VERSION', '0.1.0' );
 
 
 /*
-* Load my own woo-load-textdomain function
-* Thanks to @grappler for his help
-*/
-
+ * Load my own woo-load-textdomain function
+ * Thanks to @grappler for his help
+ */
 add_action( 'after_setup_theme', 'woo_load_textdomain', 10 );
 function woo_load_textdomain(){
-  $locale = get_locale();
-  $domain = 'woothemes';
+	$domain = 'woothemes';
 
-  if ( $loaded = load_theme_textdomain( $domain, WP_LANG_DIR . '/scrollider/' ) ) {
-    return $loaded;
-  } elseif ( $loaded = load_theme_textdomain( $domain, get_stylesheet_directory() . '/languages/' ) ) {
-    return $loaded;
-  } else {
-    load_theme_textdomain( $domain, get_template_directory() . '/languages/' );
-  }
+	if ( $loaded = load_theme_textdomain( $domain, WP_LANG_DIR . '/scrollider/' ) ) {
+		return $loaded;
+	} elseif ( $loaded = load_theme_textdomain( $domain, get_stylesheet_directory() . '/languages/' ) ) {
+		return $loaded;
+	} else {
+		load_theme_textdomain( $domain, get_template_directory() . '/languages/' );
+	}
 
-  if( is_child_theme() ) {
-    $domain = 'scrollider-child';
-    $mofile = get_stylesheet_directory() . "/languages/{$domain}-{$locale}.mo";
-    load_textdomain( $domain, $mofile );
-  }
-
+}
+/*
+ * Load child theme textdomain function
+ * Thanks to @grappler for his help
+ */
+add_action( 'after_setup_theme', 'scrollider_child_load_textdomain' );
+function scrollider_child_load_textdomain(){
+	$domain = 'scrollider-child';
+	load_theme_textdomain( $domain, get_stylesheet_directory() . '/languages/' . $domain . '/' );
 }
 
 /**
  * Load theme updater functions.
  * Thanks to @devinsays
  */
-
-function scrollider_child_theme_updater() {
-  require( get_stylesheet_directory() . '/updater/theme-updater.php' );
-}
 add_action( 'after_setup_theme', 'scrollider_child_theme_updater' );
+function scrollider_child_theme_updater() {
+	require( get_stylesheet_directory() . '/updater/theme-updater.php' );
+}
